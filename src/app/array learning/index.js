a91b4f2dd4cdad6f9a825d2b7869ca5b8f21eb67
PrikPhao <137915799+Pr1kPhao@users.noreply.github.com>
2024-02-13 12:03:36 +0700

    array

---
+++ src/app/array learning/index.js
@@ -0,0 +1,9 @@
+// let name = ['John', 'Jane', 'Marry'];
+// let lastname = name.map(val => val + 'smith');
+
+// console.log(lastname);
+
+// let numbers = [1, 2, 3, 4];
+// let multiplyNumbers = numbers.map (num => num * 2);
+
+// console.log(multiplyNumbers);
